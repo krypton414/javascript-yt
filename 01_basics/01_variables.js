4acdd1a07e3b72dedbba9fd3648211b177273c6e
krypton414 <149059964+krypton414@users.noreply.github.com>
2024-03-18 09:29:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 56748993; 
+// const using for value assign and value are no changeable if you decleare a value in const
+let accountEmail = "Piyush.01@goggle.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+
+//  accountId=23 "Not allowed"
+
+accountEmail="piyush@goggle.com"
+accountPassword = "2323232"
+accountCity = "Agra"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity]);
+// console.table is using for a multile variable console in single line 
+
