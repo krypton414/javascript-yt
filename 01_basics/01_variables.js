5b7f28cc452c9f39795b4ee9ae79b1b1cb5caa1f
krypton414 <149059964+krypton414@users.noreply.github.com>
2024-03-18 10:26:57 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,13 @@
 const accountId = 56748993; 
 // const using for value assign and value are no changeable if you decleare a value in const
+
 let accountEmail = "Piyush.01@goggle.com"
+// using a let and const in the javascript because they do not have blocl scope problem 
+//  so mainly javascript using a const and let (Block scope)
+
+
 var accountPassword = "12345"
+// it can be re-assigned a value and it can also be declared again on (global scope)
 accountCity = "Noida"
 
 //  accountId=23 "Not allowed"
@@ -12,6 +18,6 @@ accountCity = "Agra"
 
 console.log(accountId);
 
-console.table([accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
 // console.table is using for a multile variable console in single line 
 
