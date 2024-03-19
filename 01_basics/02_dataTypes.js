4d11258f945f47d1eef01c018cb37de547422204
krypton414 <149059964+krypton414@users.noreply.github.com>
2024-03-19 06:33:12 +0000

    container config done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,17 @@
  "use strict";  // treat all  JS code as a newer version 
 
 //  alert(3+3) // we are using node js, not browser
+
+let name = 'piyush'
+let age = 18
+let isLoggedin= false
+let state;
+
+
+
+// number => 2 power to 53
+//  bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
