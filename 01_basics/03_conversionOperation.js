eb89b4f27b23e419a824096d4255a928debcb2f8
krypton414 <149059964+krypton414@users.noreply.github.com>
2024-03-19 10:33:19 +0000

    conversionoperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 , false => 0
+
+/*
+it is the conversionoperation of the boolean string to boolean
+*/
+
+let isLoggedIn = "Piyush"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// true => 1; false => 0
+//  " " => false
+// "Piyush" => true
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
