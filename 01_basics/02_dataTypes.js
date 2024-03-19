988b50f0d50f8c2e529d3009442a77460b601b0f
krypton414 <149059964+krypton414@users.noreply.github.com>
2024-03-19 06:57:00 +0000

    container config done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,12 +2,21 @@
 
 //  alert(3+3) // we are using node js, not browser
 
+console.log(3+3);
+
+
+
 let name = 'piyush'
 let age = 18
 let isLoggedin= false
 let state;
 
+console.log(name);
 
+/*
+these all are the primitive data type there are two type
+of data types primitive and non-primitive
+*/
 
 // number => 2 power to 53
 //  bigint
@@ -15,3 +24,8 @@ let state;
 // boolean => true/false
 // null => standalone value
 // undefine =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Piyush");
