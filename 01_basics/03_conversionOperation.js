a5b90590ccb533c40c0f6abaf9286fcfaaedf7f2
krypton414 <149059964+krypton414@users.noreply.github.com>
2024-03-20 07:00:19 +0000

    datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -55,13 +55,14 @@ let str3 = str1 + str2
 // console.log("2" + 3); //adding a string and number vice versa also are different  
 // console.log("2" + 3 + 3);
 // console.log(2 + 3 + "3");
+console.log("2" - 3 + 2);
 
 
 // console.log((3+4)* 7 + 4 % 6);
 
 
-console.log(+true);
-console.log(+"");
+// console.log(+true);
+// console.log(+"");
 
 
 let num1, num2, num3
@@ -71,4 +72,4 @@ num1 = num2 = num3 = 2 + 2
 let gameCounter = 100
 gameCounter++;
 
-console.log(gameCounter);
+// console.log(gameCounter);
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,19 @@
+// Primitives
+
+const score = 100;
+const scoreValue = 100.3;
+let isLoggedIn = false;
+let outSideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 2332211331233n;
+// 7 types : String, boolean, null, undefined, Number, Symbol, BigInt
+
+// Referance (Non primitive)
+
+// Arrays, Objects, Functions
